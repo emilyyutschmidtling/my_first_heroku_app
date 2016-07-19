45d27f173c2a37b83f60ced070d15ed2d20ed135
Emily Schmidtling <eyschmidtling@gmail.com>
2016-07-19 12:15:44 -0700

    add puppy resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puppies = Puppy.create([{name: "Spot", age: 2, breed: "labrador"}, {name: "Dug", age: 3, breed: "mixed"}, {name: "Snoopy", age: 5, breed: "beagle"}])
